c8cb570968d4a0dc58e3fbd48e7b69708a2045f1
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-08-31 08:16:23 +0700

    feature(ed25519): update ed25519 repository

--- core_crypto/src/main/java/com/github/fadlurahmanfdev/kotlin_core_crypto/data/repositories/CryptoED25519Repository.kt
+++ core_crypto/src/main/java/com/github/fadlurahmanfdev/kotlin_core_crypto/data/repositories/CryptoED25519Repository.kt
@@ -5,9 +5,18 @@ import com.github.fadlurahmanfdev.kotlin_core_crypto.data.model.CryptoKey
 interface CryptoED25519Repository {
     fun generateKey(): CryptoKey
 
-    fun generateSignature(plainText: String, encodedPrivateKey: String): String?
+    fun generateSignature(
+        /**
+         * encoded private key, get from generateKey().privateKey
+         * @see CryptoED25519Repository.generateKey
+         * */
+        privateKey: String,
+        plainText: String,
+    ): String?
 
-    // TODO(dev): change signature to encoded signature
-    // TODO(dev): change text to plain text
-    fun verifySignature(text: String, signature: String, encodedPublicKey: String): Boolean
+    fun verifySignature(
+        publicKey: String,
+        plainText: String,
+        signature: String,
+        ): Boolean
 }
